440240aea6658f8160f10f3c203ebe1243a98fb8
+ <+>
2023-06-05 12:09:55 +0300

    fixing hydration errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar/Navbar'
 import './globals.css'
 import { Nunito } from 'next/font/google'
+import ClientOnly from '@/components/ClientOnly'
 
 
 const nunito = Nunito({ subsets: ['latin'] })
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunito.className}>
-        <Navbar />
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
